d7f738adf347e513d7265b93dc09bc8538c5f526
spwebcraft <spwebcraft@gmail.com>
2017-09-11 12:43:00 -0500

    initial commit of component entry point

---
+++ src/components/index.jsx
@@ -0,0 +1,6 @@
+import { GameComponent } from './gameComponent'
+import { HistoryComponent } from './historyComponent'
+import { MoveComponent } from './moveComponent'
+import { StatsComponent } from './statsComponent'
+
+module.exports = { GameComponent, HistoryComponent, MoveComponent, StatsComponent }
