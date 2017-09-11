8a8fd79ec3b5013d779a02d500966df39996735c
spwebcraft <spwebcraft@gmail.com>
2017-09-11 12:41:09 -0500

    initial commit of container entry point

---
+++ src/containers/index.jsx
@@ -0,0 +1,7 @@
+import { GameContainer } from './gameContainer'
+import { HistoryContainer } from './historyContainer'
+import { GlobalContainer } from './globalContainer'
+import { MoveContainer } from './moveContainer'
+import { PlayerContainer } from './playerContainer'
+
+module.exports = { GameContainer, GlobalContainer, HistoryContainer, MoveContainer, PlayerContainer }
